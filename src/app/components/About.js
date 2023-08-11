51de8217da0b9f8dfa5525abac5843d4f350acc6
AdnanRohmatKurniansah <atnandimas@gmail.com>
2023-08-11 22:47:09 +0700

    make about page

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -2,23 +2,23 @@ import AOS from "aos"
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
 
-const About = () => {
+const About = ({ darkMode }) => {
     useEffect(() => {
         AOS.init();
     })
     return (
-        <div className="bg-base-300 text-black text-center flex flex-col justify-center min-h-screen">
-    <div className="mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-        <div className="w-3/5 mx-auto" data-aos="fade-down" data-aos-duration="2000" data-aos-once="true">
-            <img src="/assets/developer.png" style={{ width: '100%' }} />
-        </div>
-        <div className="about w-4/5 text-left flex flex-col justify-center" data-aos="fade-up" data-aos-duration="2000" data-aos-once="true">
-            <h1 className="text-5xl font-bold">About Me</h1>
-            <p className="mt-4 text-xl">Hello, my name is Adnan Rohmat Kurniansah. I'm a Full Stack Web Developer Enthusiast. I'm currently studying software engineering. I like technologies that's why I'm choosing software engineering as my major. Web technologies is my favorite subject, espicially at back-end, but i would like to create front-end too.</p>
+    <div className={`${darkMode ? "bg-dark text-white" : "bg-base-300"} about px-5 md:px-0 text-center flex flex-col justify-center min-h-screen`} id="about">
+        <div className="container mx-auto mt-8 md:grid md:grid-cols-3">
+            <div className="w-3/5 md:w-80 mx-auto" data-aos="fade-down" data-aos-duration="1000" data-aos-once="true">
+                <img src="/assets/developer.png" />
+            </div>
+            <div className="col-span-2 text-left flex flex-col justify-center" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
+                <h1 className="text-5xl font-bold py-3">About Me</h1>
+                <span className="w-full h-1 rounded bg-gradient-to-r from-rose-400 via-sky-500 to-cyan-500"></span>
+                <p className="mt-4 text-xl text-justify">Hi, I'm Adnan Rohmat Kurniansah, a Software Engineer with expertise in web development. Currently, I can work as a Back-end Developer or Front-End Developer. I am a creative problem solver with a passion for building intuitive, user-friendly products that enhance the user experience.</p>
+            </div>
         </div>
     </div>
-    <i className="text-black text-4xl mt-20 fa-sharp fa-solid fa-arrow-down fa-fade"></i>
-</div>
 
     )
 } 
