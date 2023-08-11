476089e557d57f76f046dc93e06444814ecc9e7e
AdnanRohmatKurniansah <atnandimas@gmail.com>
2023-08-11 22:44:30 +0700

    make intro part

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -1,38 +1,48 @@
+import { Link } from 'react-scroll'
 import Typewriter from 'typewriter-effect'
 
-const Hero = () => {
+const Hero = ({ darkMode }) => {
     return (
-    <div className="hero bg-transparent text-white text-center flex flex-col justify-center min-h-screen">
-        <div className="mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-            <div className='imgMobile w-32 image-full mx-auto' style={{ display: 'none' }}>
-                <img src="/assets/meow.png" />
-            </div>
-            <div className="intro ">
-            <h1 className="text-5xl font-bold">Hello there, I'm 
-                <span className='text-gray-300'>
-                <Typewriter
-                options={{
-                    strings: ['Adnan', 'WebDev', 'Student'],
-                    autoStart: true,
-                    loop: true,
-                }}
-                />
-                </span></h1>
-            <p className="mt-3 text-lg">Iam a junior web developer. I can work in both backend and frontend</p>
-            <ul className="list-none flex text-3xl mt-3">
-                <li><i className="fa-brands fa-square-facebook mr-3"></i></li>
-                <li><i className="fa-brands fa-square-instagram mr-3"></i></li>
-                <li><i className="fa-brands fa-square-github mr-3"></i></li>
-                <li><i className="fa-brands fa-square-twitter mr-3"></i></li>
-            </ul>
-            </div>
-            <div className="w-80 image-full mx-auto">
-                <img src="/assets/meow.png" />
-            </div>
+    <div className={`hero bg-transparent flex flex-col justify-center  min-h-screen ${darkMode ? "text-white" : ""}`}>
+    <div className="container mx-auto md:grid md:grid-cols-2">
+      <div className="w-2/5 mt-10 md:mt-0 md:w-56 image-full mb-5 mx-auto flex justify-center items-center">
+        <img className='rounded-full bg-gray-900' src="/assets/pp.jpg" width="100%"/>
+      </div>
+      <div className="intro my-auto text-center md:text-left">
+        <h1 className="text-5xl font-bold">
+          Hello there, I'm
+          <span className={`${darkMode ? "text-blue-300" : "text-blue-700"}`}>
+            <Typewriter
+              options={{
+                strings: ["Adnan", "WebDev", "Student"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </span>
+        </h1>
+        <p className="mt-7 text-xl">
+          My expertise lies in web development and I have a understanding
+          of both front-end and back-end technologies. I have a deep interest in
+          technology and it drives me to constantly seeking opportunities to
+          expand my knowledge and improve my skills.
+        </p>
+        <div className="icons flex flex-grow gap-3 text-4xl mt-3 mx-auto">
+          <a href='https://www.linkedin.com/in/adnan-rohmat-kurniansah-41576827a/' target='_blank'><i className="fa-brands fa-linkedin"></i></a>
+          <a href='https://github.com/AdnanRohmatKurniansah' target='_blank'><i className="fa-brands fa-square-github"></i></a>
+          <a href='https://www.instagram.com/adn.rk_/' target='_blank'><i className="fa-brands fa-square-instagram"></i></a>
         </div>
-        <i className="text-white text-4xl mt-20 fa-sharp fa-solid fa-arrow-down fa-fade"></i>
+      </div>
     </div>
-
+    <Link
+      to="about"
+      spy={true}
+      smooth={true}
+      duration={1000}
+    >
+      <i className="text-4xl mt-20 fa-sharp fa-solid fa-arrow-down fa-fade"></i>
+    </Link>
+    </div>
     )
 }
 export default Hero
--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,32 +1,36 @@
-const Navbar = () => {
+import { Link } from "react-scroll";
+
+const Navbar = ({ darkMode, toggleMode }) => {
   return (
-    <div className="navbar bg-white font-bold">
+    <div className={`navbar ${darkMode ? "navBg-dark" : "bg-white"}`}>
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" className={`h-5 w-5 ${darkMode ? "text-white" : "text-black"}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
             </svg>
           </label>
-          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>About</a></li>
-            <li><a>Tools</a></li>
-            <li><a>Contact</a></li>
+          <ul tabIndex={0} className={`menu menu-compact dropdown-content mt-3 p-2 shadow ${darkMode ? "text-white bg-blue-950" : "text-black bg-base-100"} rounded-box w-52`}>
+            <li><Link to="about" spy={true} smooth={true} duration={1000}>About</Link></li>
+            <li><Link to="projects" spy={true} smooth={true} duration={1000}>Projects</Link></li>
+            <li><Link to="contact" spy={true} smooth={true} duration={1000}>Contact</Link></li>
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">
-          <i className="fa-solid fa-cannabis" style={{ fontSize: '45px' }}></i>
+        <a className={`btn btn-ghost normal-case ${darkMode ? "text-white" : "text-black"} text-3xl`}>
+          ARK.
         </a>
       </div>
-      <div className="navbar-center hidden lg:flex">
+      <div className={`navbar-center hidden lg:flex ${darkMode ? "text-white navBg-dark" : "text-black bg-white"}`}>
         <ul className="menu menu-horizontal px-1">
-          <li><a>About</a></li>
-          <li><a>Projects</a></li>
-          <li><a>Contact</a></li>
+          <li><Link to="about" spy={true} smooth={true} duration={1000}>About</Link></li>
+          <li><Link to="projects" spy={true} smooth={true} duration={1000}>Projects</Link></li>
+          <li><Link to="contact" spy={true} smooth={true} duration={1000}>Contact</Link></li>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Get started</a>
+        <i class={`${darkMode ? "text-white" : "text-black"} fa-solid fa-cloud-sun mr-2`}></i>
+        <input type="checkbox" onChange={toggleMode} checked={darkMode} className="toggle" />
+        <i class={`${darkMode ? "text-white" : "text-black"} fa-solid fa-cloud-moon ml-2`}></i>
       </div>
     </div>
   );
